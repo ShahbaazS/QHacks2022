ec280edf399826678e8338c43e97b21d3fa7e2ab
Nilushanth Thiruchelvam <nilu.thiru@gmail.com>
2022-01-29 21:06:33 -0500

    added the funtionality for adding the course review.

--- university.js
+++ university.js
@@ -1,7 +1,7 @@
 class University{
     constructor(uniChosen){
         this.uniChosen = uniChosen;
-        document.getElementById("university_chosen").innerHTML = this.uniChosen;
+        document.getElementById("university_chosen").innerHTML = this.uniChosen + " course review";
     }
 
 
@@ -12,9 +12,14 @@ let uniChosen = localStorage.getItem("uniChosen"); //the uniChosen obtained from
 const university = new University(uniChosen);
 
 function addReview(){
-    let courseName = "Course Name: " + document.getElementById("course").innerHTML +"\n";
-    let courseReview = "Review: " + document.getElementById("review").innerHTML + "\n";
+    let courseName = "Course Name: " + document.getElementById("course").value +"\n"; //using DOM method with .value to get the user entered course in the text area.
+    console.log(courseName);
+    let courseReview = "Review: " + document.getElementById("review").value + "\n\n\n"; //using DOM method with .value to get the user entered review in the text area.
+    console.log(courseReview);
+    let review = courseName + courseReview;
 
-    
+    document.getElementById("course_review").innerHTML += (review);
+
+    document.getElementById("submit").disabled = true; //disable the button now that the user entered their course review.
 
 }
